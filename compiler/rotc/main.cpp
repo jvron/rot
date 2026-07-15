7f1d40452a0bd73d2e7059263c9fe27c069b0d02
jvron <205451073+jvron@users.noreply.github.com>
2026-07-15 23:31:57 +0100

    feat(rotc): add command line argument handling and file loading

--- compiler/rotc/main.cpp
+++ compiler/rotc/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 
-int main(void) {
+#include "core/exit_code.hpp"
+#include "core/result.hpp"
+#include "util/file.hpp"
 
-    return 0;
+int main(int argc, char* argv[]) {
+
+    if (argc < 2) {
+        std::cerr << "Usage: rotc <file> \n";
+        return to_int(ExitCode::UsageError);
+    }
+
+    std::string path = argv[1];
+    Result<std::string> result = File::read(path);
+
+    if (!result) {
+        std::cerr << result.error.message << "\n";
+        return to_int(ExitCode::FileError);
+    }
+
+    return to_int(ExitCode::Success); 
 }
