16cafbfce9e70c2b440aecea057fe3e79cfb60ee
jvron <205451073+jvron@users.noreply.github.com>
2026-07-16 20:00:41 +0100

    feat(lexer): add TokenType enum and Token data structure

---
+++ compiler/rotc/lexer/token.hpp
@@ -0,0 +1,40 @@
+#pragma once 
+
+#include <string>
+
+enum class TokenType {
+    // single character
+    LeftParen, RightParen, LeftBrace, RightBrace,
+    Comma, Dot, Minus, Plus, SemiColon, Slash, Star,  
+
+    // one - two characters
+    Eqaul, EqualEqual, 
+    Bang, BangEqual, 
+    Greater, GreaterEqual,
+    Less, LessEqual,
+
+    // literal 
+    Indentifier, String, Integer, Float, Character, 
+
+    // keywords
+    If, Else, 
+    While, For,
+    True, False,
+    And, Or,
+    Null, 
+    Return, 
+    Function,
+    Print,  
+
+    Eof 
+};
+
+struct Token {
+    TokenType type;
+    std::string lexeme; 
+    int line {};
+
+    Token() {};
+    Token(TokenType token_type, std::string token_lexeme, int token_line)
+        : type(token_type), lexeme(std::move(token_lexeme)), line(token_line) {};
+};
