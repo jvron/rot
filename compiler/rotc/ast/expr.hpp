ea2e1a792a67fddc4b3fb3f9d83c9cb9ef4ad375
jvron <205451073+jvron@users.noreply.github.com>
2026-07-18 16:04:14 +0100

    feat(ast): add binary, unary, literal, and grouping expression nodes

---
+++ compiler/rotc/ast/expr.hpp
@@ -0,0 +1,34 @@
+#pragma once 
+
+#include <memory>
+#include <variant>
+
+#include "lexer/token.hpp"
+
+struct Expr;
+
+struct BinaryExpr {
+    std::unique_ptr<Expr> left;
+    Token op;
+    std::unique_ptr<Expr> right;
+};
+
+struct UnaryExpr {
+    Token op;
+    std::unique_ptr<Expr> right;
+};
+
+struct LiteralExpr {
+    Token literal;
+};
+
+struct GroupingExpr {
+    std::unique_ptr<Expr> expr;
+};
+
+struct Expr {
+    std::variant<
+        BinaryExpr, UnaryExpr, 
+        LiteralExpr, GroupingExpr
+        > node;
+};
