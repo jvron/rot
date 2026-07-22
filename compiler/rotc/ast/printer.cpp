f87854514049bb8bbbb1ff6cefc47d950e218a10
jvron <205451073+jvron@users.noreply.github.com>
2026-07-22 16:35:25 +0100

    feat(printer): add Program AST and statement printing

--- compiler/rotc/ast/printer.cpp
+++ compiler/rotc/ast/printer.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 
 #include "printer.hpp"
+#include "ast/ast.hpp"
 #include "ast/expr.hpp"
-#include "parser/parser.hpp"
+#include "ast/stmt.hpp"
 
 struct ExprPrinter {
 
@@ -34,12 +35,36 @@ struct ExprPrinter {
         std::cout << "Literal expr(" << expr.literal.lexeme  << ")" << "\n";
     }
 
+    void operator()(const IdentifierExpr& expr) {
+        Printer::print_indent(indent);
+
+        std::cout << "Identifier expr(" << expr.name.lexeme  << ")" << "\n";
+    }
+
     void operator()(const GroupingExpr& expr) {
         Printer::print_indent(indent);
 
         std::cout << "Grouping expr: " << "\n";
 
         Printer::print_expr(*expr.expr, indent + 1);
+    }
+};
+
+struct StmtPrinter {
+    int indent = 0;
+    StmtPrinter(int indent) : indent(indent) {};
+
+    void operator()(const VarDeclarationStmt& stmt) {
+        Printer::print_indent(indent);
+
+        std::cout << "variable declaration("<< stmt.var_name.lexeme << ")\n";
+        Printer::print_expr(*stmt.initializer, indent + 1);
+    }
+
+    void operator()(const ExprStmt& stmt) {
+        Printer::print_indent(indent);
+
+        Printer::print_expr(*stmt.expr, indent + 1);
     }
 };
 
@@ -54,6 +79,14 @@ void Printer::print_expr(const Expr& expr, int indent) {
     std::visit(printer, expr.node);
 }
 
-void Printer::print_expr_tree(const ExprTree &tree) {
-    print_expr(tree.root, 0);
+void Printer::print_stmt(const Stmt &stmt, int indent) {
+    StmtPrinter printer(indent);
+    std::visit(printer, stmt.node);
+}
+
+void Printer::print_ast(const Program& program) {
+
+    for (const auto& statement : program.statements) {
+        print_stmt(statement, 0);
+    }
 }
--- compiler/rotc/ast/printer.hpp
+++ compiler/rotc/ast/printer.hpp
@@ -1,10 +1,13 @@
 #pragma once 
 
+#include "ast/ast.hpp"
 #include "ast/expr.hpp"
+#include "ast/stmt.hpp"
 #include "parser/parser.hpp"
 
 namespace Printer {
     void print_indent(int indent);
     void print_expr(const Expr& expr, int indent);
-    void print_expr_tree(const ExprTree& tree);
+    void print_stmt(const Stmt& stmt, int indent);
+    void print_ast(const Program& program);
 }
