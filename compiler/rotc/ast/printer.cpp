e6c143e51464daac301694a9d8376609c5b7cf8a
jvron <205451073+jvron@users.noreply.github.com>
2026-07-23 14:33:34 +0100

    refactor(printer): convert printer to a class
    - Encapsulate printer state and helper methods
    - Nest AST visitors inside Printer
    - Extract lexemes from source spans
    - Add assignment statement printing

--- compiler/rotc/ast/printer.cpp
+++ compiler/rotc/ast/printer.cpp
@@ -6,65 +6,76 @@
 #include "ast/expr.hpp"
 #include "ast/stmt.hpp"
 
-struct ExprPrinter {
+struct Printer::ExprPrinter {
 
+    Printer& printer;
     int indent = 0;
 
-    ExprPrinter(int indent) : indent(indent) {};
+    ExprPrinter(Printer& printer, int indent) : printer(printer), indent(indent) {}
 
     void operator()(const BinaryExpr& expr) {
-        Printer::print_indent(indent);
+        printer.print_indent(indent);
 
-        std::cout << "Binary expr(" << expr.op.lexeme << ")" << "\n";
+        std::cout << "Binary expr(" << printer.get_lexeme(expr.op) << ")" << "\n";
 
-        Printer::print_expr(*expr.left, indent + 1);
-        Printer::print_expr(*expr.right, indent + 1);
+        printer.print_expr(*expr.left, indent + 1);
+        printer.print_expr(*expr.right, indent + 1);
     }
 
     void operator()(const UnaryExpr& expr) {
-        Printer::print_indent(indent);
+        printer.print_indent(indent);
 
-        std::cout << "Unary expr(" << expr.op.lexeme << ")" << "\n";
+        std::cout << "Unary expr(" << printer.get_lexeme(expr.op) << ")" << "\n";
 
-        Printer::print_expr(*expr.right, indent + 1);
+        printer.print_expr(*expr.right, indent + 1);
     }
 
     void operator()(const LiteralExpr& expr) {
-        Printer::print_indent(indent);
+        printer.print_indent(indent);
 
-        std::cout << "Literal expr(" << expr.literal.lexeme  << ")" << "\n";
+        std::cout << "Literal expr(" << printer.get_lexeme(expr.literal) << ")" << "\n";
     }
 
     void operator()(const IdentifierExpr& expr) {
-        Printer::print_indent(indent);
+        printer.print_indent(indent);
 
-        std::cout << "Identifier expr(" << expr.name.lexeme  << ")" << "\n";
+        std::cout << "Identifier expr(" << printer.get_lexeme(expr.name) << ")" << "\n";
     }
 
     void operator()(const GroupingExpr& expr) {
-        Printer::print_indent(indent);
+        printer.print_indent(indent);
 
         std::cout << "Grouping expr: " << "\n";
 
-        Printer::print_expr(*expr.expr, indent + 1);
+        printer.print_expr(*expr.expr, indent + 1);
     }
-};
+}; 
 
-struct StmtPrinter {
+struct Printer::StmtPrinter {
+
+    Printer& printer;
     int indent = 0;
-    StmtPrinter(int indent) : indent(indent) {};
+
+    StmtPrinter(Printer& printer, int indent) : printer(printer), indent(indent){}
 
     void operator()(const VarDeclarationStmt& stmt) {
-        Printer::print_indent(indent);
+        printer.print_indent(indent);
 
-        std::cout << "variable declaration("<< stmt.var_name.lexeme << ")\n";
-        Printer::print_expr(*stmt.initializer, indent + 1);
+        std::cout << "variable declaration("<< printer.get_lexeme(stmt.var_name) << ")\n";
+        printer.print_expr(*stmt.initializer, indent + 1);
+    }
+
+    void operator()(const AssignmentStmt& stmt) {
+        printer.print_indent(indent);
+
+        std::cout << "variable assignment("<< printer.get_lexeme(stmt.var_name) << ")\n";
+        printer.print_expr(*stmt.initializer, indent + 1);
     }
 
     void operator()(const ExprStmt& stmt) {
-        Printer::print_indent(indent);
+        printer.print_indent(indent);
 
-        Printer::print_expr(*stmt.expr, indent + 1);
+        printer.print_expr(*stmt.expr, indent + 1);
     }
 };
 
@@ -74,13 +85,17 @@ void Printer::print_indent(int indent) {
     } 
 }
 
+std::string Printer::get_lexeme(const Token& token) {
+    return source.substr(token.span.start.offset, token.span.end.offset - token.span.start.offset);
+}
+
 void Printer::print_expr(const Expr& expr, int indent) {
-    ExprPrinter printer(indent);
+    ExprPrinter printer(*this, indent);
     std::visit(printer, expr.node);
 }
 
 void Printer::print_stmt(const Stmt &stmt, int indent) {
-    StmtPrinter printer(indent);
+    StmtPrinter printer(*this, indent);
     std::visit(printer, stmt.node);
 }
 
--- compiler/rotc/ast/printer.hpp
+++ compiler/rotc/ast/printer.hpp
@@ -3,11 +3,24 @@
 #include "ast/ast.hpp"
 #include "ast/expr.hpp"
 #include "ast/stmt.hpp"
-#include "parser/parser.hpp"
 
-namespace Printer {
+
+class Printer {
+
+private:
+    const std::string& source;
+
+    struct ExprPrinter;
+    struct StmtPrinter;
+
     void print_indent(int indent);
+    std::string get_lexeme(const Token& token);
+
     void print_expr(const Expr& expr, int indent);
     void print_stmt(const Stmt& stmt, int indent);
+
+public:
+    Printer(const std::string& src) : source(src) {}
+
     void print_ast(const Program& program);
-}
+};
